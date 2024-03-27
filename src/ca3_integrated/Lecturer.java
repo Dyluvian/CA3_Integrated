ecf34eb2f04d15497271b4584a65faa1197ba51d
Dyluvian <152575690+Dyluvian@users.noreply.github.com>
2024-03-27 09:35:19 +0000

    Lecturer functionality
    Insert the same for Lecturer

--- src/ca3_integrated/Lecturer.java
+++ src/ca3_integrated/Lecturer.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ca3_integrated;
 
-/**
- *
- * @author User
- */
-public class Lecturer {
-    
+public class Lecturer extends Staff {
+
+    public Lecturer(int userID, String username, String password) {
+        super(userID, username, password, "Lecturer");
+    }
 }
