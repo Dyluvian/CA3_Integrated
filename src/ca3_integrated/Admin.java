e64a3795ea6fe8c9fa5f76868bd2b025816be30e
Dyluvian <152575690+Dyluvian@users.noreply.github.com>
2024-03-27 13:52:54 +0000

    Admin - add comments
    Let's polish and add in some comments I scribbled down for smaller methods

--- src/ca3_integrated/Admin.java
+++ src/ca3_integrated/Admin.java
@@ -9,11 +9,13 @@ public class Admin extends Staff {
 // ------------------
 // CLASS: ADMIN, SUBCLASS OF STAFF
 // ------------------
-//
+// This class represents the Admin role, referring back to the Staff superclass which it extends.
+// Nothing unique has been prescribed for the Admin or other roles in the brief for this project, save the menu they access.
 // ------------------
 */
 
-    public Admin(int userID, String username, String password) {
-        super(userID, username, password, "Admin");
+// The Admin constructor.
+    public Admin(int userID, String username, String password) { // Admin accepts a numerical user ID, and username and password strings
+        super(userID, username, password, "Admin"); // Return the variable ID, username, and password to Staff, along with the fixed role, Admin
     }
 }
