9a06d1302318bb7ce9b70bdedfc073dc0ff2c8b7
Dyluvian <152575690+Dyluvian@users.noreply.github.com>
2024-03-27 10:01:47 +0000

    Staff basic docstring
    Same again

--- src/ca3_integrated/Staff.java
+++ src/ca3_integrated/Staff.java
@@ -1,13 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ca3_integrated;
 
-/**
- *
- * @author User
- */
+/*
+// Dylan Geraghty - CA3 (Integrated CA) - Programming: Object-Oriented Approach
+// ------------------
+// https://github.com/Dyluvian/CA3_Integrated
+// ------------------
+// CLASS: STAFF
+// ------------------
+// 
+// ------------------
+*/
+
 public class Staff {
     
 }
