6e0ee5a7cd7bdfb0ff8b4c28b6cf896d9dbfa96b
Dyluvian <152575690+Dyluvian@users.noreply.github.com>
2024-03-27 13:56:56 +0000

    Office - comments
    Similar for Office too

--- src/ca3_integrated/Office.java
+++ src/ca3_integrated/Office.java
@@ -7,13 +7,15 @@ package ca3_integrated;
 // ------------------
 // CLASS: OFFICE, SUBCLASS OF STAFF
 // ------------------
-//
+// This class represents the Office role, referring back to the Staff superclass which it extends.
+// Nothing unique has been prescribed for the Office(r?) or other roles in the brief for this project, save the menu they access.
 // ------------------
 */
 
 public class Office extends Staff {
 
-    public Office(int userID, String username, String password) {
-        super(userID, username, password, "Office");
+// The Office constructor.
+    public Office(int userID, String username, String password) { // Office accepts a numerical user ID, and username and password strings
+        super(userID, username, password, "Office"); // Return the variable ID, username, and password to Staff, along with the fixed role, Office
     }
 }
