1314fb68f9bbb360fee360b1bd243c54ca727163
Dyluvian <152575690+Dyluvian@users.noreply.github.com>
2024-03-27 08:18:44 +0000

    Elaborated welcome message
    Don't foolishly forget about the more extensive welcome message

--- src/ca3_integrated/CA3_Integrated.java
+++ src/ca3_integrated/CA3_Integrated.java
@@ -6,7 +6,7 @@ public static void main(String[] args) {
         UserDatabase userDatabase = new UserDatabase(); 
         LoginView loginView = new ConsoleLoginView(); 
         UserController userController = new ConsoleUserController(loginView, userDatabase); 
-        System.out.println("---\nWelcome."); 
+        System.out.println("---\nWelcome to the Report Generation Framework for Innistown College, the most fictional college in Ireland.\nYou are liable to have veritable hours of fun adding, editing, and deleting your colleagues, logging in and out and in again, and snooping on others' GDPR-protected details.\nDo enjoy the experience and genuinely hope to see you again during the next trimester."); 
         userController.login(); 
     }
 }
