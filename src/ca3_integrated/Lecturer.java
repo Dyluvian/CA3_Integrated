6d29b014c80213a6f0c1f3b9591c336e05642a4b
Dyluvian <152575690+Dyluvian@users.noreply.github.com>
2024-03-27 13:54:52 +0000

    Lecturer - comments
    Very similar to Admin

--- src/ca3_integrated/Lecturer.java
+++ src/ca3_integrated/Lecturer.java
@@ -7,13 +7,15 @@ package ca3_integrated;
 // ------------------
 // CLASS: LECTURER, SUBCLASS OF STAFF
 // ------------------
-//
+// This class represents the Lecturer role, referring back to the Staff superclass which it extends.
+// Nothing unique has been prescribed for the Lecturer or other roles in the brief for this project, save the menu they access.
 // ------------------
 */
 
 public class Lecturer extends Staff {
 
-    public Lecturer(int userID, String username, String password) {
-        super(userID, username, password, "Lecturer");
+// The Lecturer constructor.
+    public Lecturer(int userID, String username, String password) { // Lecturer accepts a numerical user ID, and username and password strings
+        super(userID, username, password, "Lecturer"); // Return the variable ID, username, and password to Staff, along with the fixed role, Lecturer
     }
 }
