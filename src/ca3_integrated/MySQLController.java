581cfdc0b744ef1b5c8575c6c3d071f4f36fd9a7
Dyluvian <152575690+Dyluvian@users.noreply.github.com>
2024-03-27 19:34:44 +0000

    MySQLConnector - ResultSet stuff

--- src/ca3_integrated/MySQLController.java
+++ src/ca3_integrated/MySQLController.java
@@ -22,5 +22,15 @@ public class MySQLController {
     private final String DB_URL = "jdbc:mysql://localhost/innistown_college";
     private final String USERNAME = "pooa2024";
     private final String PASSWORD = "pooa2024";
-    
+
+    public ResultSet executeQuery(String query) {
+        try {
+            Connection connection = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+            Statement statement = connection.createStatement();
+            return statement.executeQuery(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }    
 }
