d0c1ca31dfec3e6e99d3297908bf0df2898d918d
Dyluvian <152575690+Dyluvian@users.noreply.github.com>
2024-03-27 21:23:54 +0000

    Lecturer and Office Controllers - add commentary
    Commentating these two controllers, time is precious

--- src/ca3_integrated/LecturerController.java
+++ src/ca3_integrated/LecturerController.java
@@ -7,67 +7,70 @@ package ca3_integrated;
 // ------------------
 // CLASS: USER CONTROLLER FOR LECTURER ROLE (LECTURERCONTROLLER)
 // ------------------
-// 
+// This class delves into the unique functionalities assigned to the Lecturer role, beyond the generic user coverage of the main User Controller.
+// Specifically, it enables the Lecturer to view a Lecturer Report, modify their own username and password, log out, and close the program.
 // ------------------
 */
 
-import java.util.Scanner;
+import java.util.Scanner; // import Scanner for accepting user input
 
 public class LecturerController implements UserController {
 
-    private UserDatabase userDatabase;
+    private UserDatabase userDatabase; // initialise the database
 
     public LecturerController(UserDatabase userDatabase) {
-        this.userDatabase = userDatabase;
+        this.userDatabase = userDatabase; // relate the AdminController to the database
     }
     
     @Override
-    public void login() {
+    public void login() { // Standard login
     }
     
     @Override
     public void showMenu(User user) {
-        if (user instanceof Lecturer) {
-            showLecturerMenu((Lecturer) user);
+        if (user instanceof Lecturer) { // if the user is a Lecturer...
+            showLecturerMenu((Lecturer) user); // ...show the Lecturer menu
         }
     }
     
     public void showLecturerMenu(Lecturer lecturer) {
-        Scanner scanner = new Scanner(System.in);
-        int choice;
+        Scanner scanner = new Scanner(System.in); // kickstart the scanner
+        int choice; // initialise the choice integer for switch
         do {
-            System.out.println("---\nInput 1 to do nothing for now.\nInput 2 to change your username and password.\nInput 3 to log out.\nInput 4 to close the software.\n---");
-            choice = scanner.nextInt();
+            System.out.println("---\nInput 1 to do nothing for now.\nInput 2 to change your username and password.\nInput 3 to log out.\nInput 4 to close the software.\n---"); // instructions
+            choice = scanner.nextInt(); // next int inserted defines your choice
             switch (choice) {
 // TO GENERATE A LECTURER REPORT, I'LL GET BACK TO YOU
 
 // TO CHANGE YOUR USERNAME AND PASSWORD
-                case 2:
-                    int activeLecturerID = lecturer.getUserID();
+                case 2: // if you input 2...
+                    int activeLecturerID = lecturer.getUserID(); // note the active lecturer's ID
                     User activeLecturer = userDatabase.getUser(activeLecturerID);
-                    System.out.println("---\nAcknowledged.\nInput 1 to modify your username.\nInput 2 to modify your password.");
+                    System.out.println("---\nAcknowledged.\nInput 1 to modify your username.\nInput 2 to modify your password."); // choose which action
                     System.out.println("---");
-                    int choiceCredentials = scanner.nextInt();
+                    int choiceCredentials = scanner.nextInt(); // granular choice of which credential to modify
                     switch (choiceCredentials) {
-                        case 1:
+                        case 1: // if you input 1...
                             System.out.println("---\nNow, please input the new username you would like to change over to.\n---");
-                            String newUsername = scanner.next();
-                            if (userDatabase.usernameExists(newUsername)) {
+                            String newUsername = scanner.next(); // the next input will become your username
+                            if (userDatabase.usernameExists(newUsername)) { // unless it already exists...
+                                System.out.println("---\nImpossible! That username is already present in the database. Please choose another."); // in which case, your ears will burn
+                            } else {
                                 System.out.println("---\nSuperlative! Your username has been updated. Renewals within renewals.");
                                 break;
                             }
-                            activeLecturer.setUsername(newUsername);
+                            activeLecturer.setUsername(newUsername); // if all is well, update the username
                             System.out.println("---\nYour username has been updated successfully.\n---");
                             break;
-                        case 2:
+                        case 2: // if you input 2...
                             System.out.println("---\nNow, please input your new password.\n---");
-                            activeLecturer.setPassword(scanner.next());
+                            activeLecturer.setPassword(scanner.next()); // the next input will become your password. Anything works
                             System.out.println("---\nSplendid! Your password has been changed. Go forth and feel secure!");
                             break;
-                        default:
+                        default: // if you input nonsense...
                             System.out.println("---\nNonsense! That is not valid input. You must choose either your username (1), or password (2).");
                     }
-                    userDatabase.updateUser(activeLecturer.getUserID(), activeLecturer);
+                    userDatabase.updateUser(activeLecturer.getUserID(), activeLecturer); // finally, update the active lecturer
                     break;
 // TO LOG OUT
                 case 3: // if the user inputs 3...
@@ -85,7 +88,7 @@ public class LecturerController implements UserController {
 
     public void logout() {
         System.out.println("---\nYou have logged out. Luckily, Innistown College has invested in automatic doors, which will not hit you on the way out.");
-        UserController controller = new ConsoleUserController(new ConsoleLoginView(), userDatabase);
-        controller.login();
+        UserController controller = new ConsoleUserController(new ConsoleLoginView(), userDatabase); // get a new User Controller going
+        controller.login(); // and return to the login screen
     }
 }
--- src/ca3_integrated/OfficeController.java
+++ src/ca3_integrated/OfficeController.java
@@ -6,101 +6,101 @@ package ca3_integrated;
 // ------------------
 // CLASS: USER CONTROLLER FOR OFFICE ROLE (OFFICECONTROLLER)
 // ------------------
-// 
+// This class delves into the unique functionalities assigned to the Office role, beyond the generic user coverage of the main User Controller.
+// Specifically, it enables the Office...r to generate reports, modify their own username and password, log out, and close the program.
 // ------------------
 */
 
 import java.util.Scanner;
 
-public class OfficeController implements UserController {
+public class OfficeController implements UserController { // import Scanner for accepting user input
 
-    private UserDatabase userDatabase;
+    private UserDatabase userDatabase; // initialise the database
 
     public OfficeController(UserDatabase userDatabase) {
-        this.userDatabase = userDatabase;
+        this.userDatabase = userDatabase; // relate the OfficeController to the database
     }
     
     @Override
-    public void login() {
+    public void login() { // Standard login
     }
 
     @Override
     public void showMenu(User user) {
-        if (user instanceof Office) {
-            showOfficeMenu((Office) user);
+        if (user instanceof Office) { // if the user is an Office... guy... or... not so guy...
+            showOfficeMenu((Office) user); // ...show the Office menu
         }
     }
 
     public void showOfficeMenu(Office office) {
-        Scanner scanner = new Scanner(System.in);
-        int choice;
+        Scanner scanner = new Scanner(System.in); // kickstart the scanner
+        int choice; // initialise the choice integer for switch
         do {
-            System.out.println("---\nInput 1 to do nothing for now.\nInput 2 to change your username and password.\nInput 3 to log out.\nInput 4 to close the software.\n---");
-            choice = scanner.nextInt();
+            System.out.println("---\nInput 1 to generate a report.\nInput 2 to change your username and password.\nInput 3 to log out.\nInput 4 to close the software.\n---");
+            choice = scanner.nextInt(); // next int inserted defines your choice
             switch (choice) {
-// TO GENERATE A REPORT?
-            case 1:
+// TO GENERATE A REPORT
+            case 1: // if you input 1...
             System.out.println("---\nAcknowledged.\nInput 1 to generate a Course Report.\nInput 2 to generate a Lecturer Report.\nInput 3 to generate a Student Report..\n---");
-                    int reportType = scanner.nextInt();
+                    int reportType = scanner.nextInt(); // next int will represent the report TYPE (Course, Lecturer etc) to output
                     scanner.nextLine();
             System.out.println("---\nUnderstood.\nInput 1 to output the report in CSV format.\nInput 2 to output it in TXT format.\nInput 3 to print the output to the console.\n---");
-                    int outputFormat = scanner.nextInt();
+                    int outputFormat = scanner.nextInt(); // next int will represent the format
                     String format;
                     switch (outputFormat) {
-                        case 1:
-                            format = "csv";
+                        case 1: // if you input 1...
+                            format = "csv"; // then csv it is
                             break;
-                        case 2:
-                            format = "txt";
+                        case 2: // if you input 2...
+                            format = "txt"; // then txt it is
                             break;
-                        case 3:
+                        case 3: // if you input 3...
                             format = "console";
                             break;
-                        default:
+                        default: // if you input nonsense...
                             System.out.println("\nThat is not a valid format! Printing to console in the absence of better ideas.");
                             format = "console";
                     }
                     switch (reportType) {
-                        case 1:
-                            generateCourseReport(format);
+                        case 1: // if you had input 1 for type...
+                            generateCourseReport(format); // generate a course report
                             break;
-                        case 2:
-                            generateLecturerReport(format);
+                        case 2: // if you had input 2 for type...
+                            generateLecturerReport(format); // generate a lecturer report
                             break;
-                        case 3:
-                            generateStudentReport(format);
+                        case 3: // if you had input 3 for type...
+                            generateStudentReport(format); // generate a student report
                             break;
-                        default:
+                        default: // if you input nonsense...
                             System.out.println("\nThat does not correspond to a valid report type!");
                     }
                     break;
 // TO CHANGE YOUR USERNAME AND PASSWORD
-                case 2:
-                    int activeOfficeID = office.getUserID();
+                case 2: // if you input 2...
+                    int activeOfficeID = office.getUserID(); // note the ID of the active user
                     User activeOffice = userDatabase.getUser(activeOfficeID);
                     System.out.println("---\nAcknowledged.\nInput 1 to modify your username.\nInput 2 to modify your password.");
                     System.out.println("---");
-                    int choiceCredentials = scanner.nextInt();
+                    int choiceCredentials = scanner.nextInt(); // granular choice of which credential to modify
                     switch (choiceCredentials) {
-                        case 1:
+                        case 1: // if you input 1...
                             System.out.println("---\nNow, please input the new username you would like to change over to.\n---");
-                            String newUsername = scanner.next();
-                            if (userDatabase.usernameExists(newUsername)) {
+                            String newUsername = scanner.next(); // the next input will become your username
+                            if (userDatabase.usernameExists(newUsername)) { // unless it already exists...
+                                System.out.println("---\nImpossible! That username is already present in the database. Please choose another."); // in which case, your ears will burn
+                            } else {
                                 System.out.println("---\nSuperlative! Your username has been updated. Renewals within renewals.");
                                 break;
                             }
-                            activeOffice.setUsername(newUsername);
-                            System.out.println("---\nYour username has been updated successfully.\n---");
-                            break;
-                        case 2:
+                        case 2: // if you input 2...
                             System.out.println("---\nNow, please input your new password.\n---");
-                            activeOffice.setPassword(scanner.next());
+                            activeOffice.setPassword(scanner.next()); // the next input will become your password. Anything works
                             System.out.println("---\nSplendid! Your password has been changed. Go forth and feel secure!");
                             break;
                         default:
                             System.out.println("---\nNonsense! That is not valid input. You must choose either your username (1), or password (2).\n---");
                     }
-                    userDatabase.updateUser(activeOffice.getUserID(), activeOffice);
+                    userDatabase.updateUser(activeOffice.getUserID(), activeOffice); // if all is well, update the user
                     break;
 // TO LOG OUT
                 case 3: // if the user inputs 3...
@@ -118,8 +118,8 @@ public class OfficeController implements UserController {
 
     public void logout() {
         System.out.println("---\nYou have logged out. Luckily, Innistown College has invested in automatic doors, which will not hit you on the way out.");
-        UserController controller = new ConsoleUserController(new ConsoleLoginView(), userDatabase);
-        controller.login();
+        UserController controller = new ConsoleUserController(new ConsoleLoginView(), userDatabase); // get a new User Controller going
+        controller.login(); // and return to the login screen
     }
 
     private void generateCourseReport(String format) {
