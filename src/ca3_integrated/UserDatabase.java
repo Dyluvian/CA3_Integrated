eca25a566f46a7109030ee667ad81d3ff8ad2d07
Dyluvian <152575690+Dyluvian@users.noreply.github.com>
2024-03-27 09:01:10 +0000

    Printing list of all users
    Add method for printing all users, which relies on an iterator.

--- src/ca3_integrated/UserDatabase.java
+++ src/ca3_integrated/UserDatabase.java
@@ -1,6 +1,7 @@
 package ca3_integrated;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class UserDatabase {
@@ -16,4 +17,21 @@ public class UserDatabase {
         users.add(user);
     }
     
+    public User getUser(int userID) {
+        for (User user : users) {
+            if (user.getUserID() == userID) {
+                return user;
+            }
+        }
+        return null;
+    }
+    
+    public void printAllUsers() {
+        Iterator<User> iterateUsers = users.iterator();
+        while (iterateUsers.hasNext()) {
+            User userToIterate = iterateUsers.next();
+            System.out.println("User ID: " + userToIterate.getUserID() + ". Username: " + userToIterate.getUsername() + ". Password: " + userToIterate.getPassword() + ". Role: " + userToIterate.getRole());
+        }
+    }
+
 }
