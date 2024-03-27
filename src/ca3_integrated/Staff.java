43999a60b6ee7a6ea0e6d2fad0d07a11026aa21a
Dyluvian <152575690+Dyluvian@users.noreply.github.com>
2024-03-27 10:29:40 +0000

    Populate Staff class
    Should really populate the Staff class as well before moving onward to user controllers etc. This was modelled around CA2 material in preparatory drafts, but abstract class took some time to get my head around

--- src/ca3_integrated/Staff.java
+++ src/ca3_integrated/Staff.java
@@ -10,7 +10,21 @@ package ca3_integrated;
 // 
 // ------------------
 */
+    
+public abstract class Staff implements User {
+    
+    private static int lastAssignedUserID = 0;
 
-public class Staff {
+    private int userID;
+    private String username;
+    private String password;
+    private String role;
+
+    public Staff(int userID, String username, String password, String role) {
+        this.userID = ++lastAssignedUserID;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
     
 }
