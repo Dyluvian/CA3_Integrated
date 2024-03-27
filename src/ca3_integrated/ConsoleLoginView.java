a549fc2aab09a460ed6aacabcda4abb8df804eb2
Dyluvian <152575690+Dyluvian@users.noreply.github.com>
2024-03-27 08:32:24 +0000

    Create ConsoleLoginView
    Create file for the Login View Console class, which is the implementation for the Login View interface

---
+++ src/ca3_integrated/ConsoleLoginView.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ca3_integrated;
+
+/**
+ *
+ * @author User
+ */
+public class ConsoleLoginView {
+    
+}
