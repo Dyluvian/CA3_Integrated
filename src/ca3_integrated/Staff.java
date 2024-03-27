6f6f6d64e528f21e44ff39dd47d6f08234197afe
Dyluvian <152575690+Dyluvian@users.noreply.github.com>
2024-03-27 11:47:47 +0000

    Staff - setRole
    As suspected, going to need this setter to complete the implementation

--- src/ca3_integrated/Staff.java
+++ src/ca3_integrated/Staff.java
@@ -60,4 +60,9 @@ public abstract class Staff implements User {
     public String getRole() {
         return role;
     }
+    
+    @Override    
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
