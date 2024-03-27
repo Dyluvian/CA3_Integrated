809c09ca1fd079c83072883468c4773f80031822
Dyluvian <152575690+Dyluvian@users.noreply.github.com>
2024-03-27 09:22:14 +0000

    Create Staff
    Create Staff, the overarching superclass of Admin, Office, and Lecturer

--- src/ca3_integrated/UserDatabase.java
+++ src/ca3_integrated/UserDatabase.java
@@ -12,11 +12,11 @@ public class UserDatabase {
         users = new ArrayList<>();
         users.add(new Admin(1, "admin", "java"));
     }
-    
+
     public void addUser(User user) {
         users.add(user);
     }
-    
+
     public User getUser(int userID) {
         for (User user : users) {
             if (user.getUserID() == userID) {
@@ -25,7 +25,7 @@ public class UserDatabase {
         }
         return null;
     }
-    
+
     public void printAllUsers() {
         Iterator<User> iterateUsers = users.iterator();
         while (iterateUsers.hasNext()) {
@@ -33,7 +33,7 @@ public class UserDatabase {
             System.out.println("User ID: " + userToIterate.getUserID() + ". Username: " + userToIterate.getUsername() + ". Password: " + userToIterate.getPassword() + ". Role: " + userToIterate.getRole());
         }
     }
-    
+
     public void updateUser(int userID, User newUser) {
         for (int i = 0; i < users.size(); i++) {
             if (users.get(i).getUserID() == userID) {
@@ -42,7 +42,7 @@ public class UserDatabase {
             }
         }
     }
-    
+
     public void deleteUser(int userIDToDelete) {
         boolean userDeleted = false;
         Iterator<User> iterateUsers = users.iterator();
@@ -56,7 +56,7 @@ public class UserDatabase {
             }
         }
     }
-    
+
     public User verifyUser(String username, String password) {
         if (users == null) {
             return null;
