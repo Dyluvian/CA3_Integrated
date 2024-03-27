0509e586b67fc4aec57d8d7e321612a40d913faf
Dyluvian <152575690+Dyluvian@users.noreply.github.com>
2024-03-27 08:14:07 +0000

    Main method
    Introduce the main method for CA3, which will be brief and non-concrete, referring to external classes and interfaces.

--- src/ca3_integrated/CA3_Integrated.java
+++ src/ca3_integrated/CA3_Integrated.java
@@ -2,7 +2,11 @@ package ca3_integrated;
 
 public class CA3_Integrated {
 
-    public static void main(String[] args) {
+public static void main(String[] args) {
+        UserDatabase userDatabase = new UserDatabase(); 
+        LoginView loginView = new ConsoleLoginView(); 
+        UserController userController = new ConsoleUserController(loginView, userDatabase); 
+        System.out.println("---\nWelcome."); 
+        userController.login(); 
     }
-    
 }
