7faad8f0a2f238e33376ac87178698ccc9125aa0
Dyluvian <152575690+Dyluvian@users.noreply.github.com>
2024-03-27 14:06:12 +0000

    User - comments
    Write in some quick comments for user

--- src/ca3_integrated/User.java
+++ src/ca3_integrated/User.java
@@ -7,25 +7,26 @@ package ca3_integrated;
 // ------------------
 // INTERFACE: USER
 // ------------------
-// 
+// This interface reprsents the user entity (as opposed to representing the actual staff member BEHIND the user; that is handled by the Staff class).
+// It draws on the implementations relevant to that individual as laid out in Staff.
 // ------------------
-*/
+ */
 
 public interface User {
-    
-    int getUserID();
-    
-    void decrementUserID();
 
-    String getUsername();
-    
-    void setUsername(String username);
-    
-    String getPassword();
-    
-    void setPassword(String password);
+    int getUserID(); // Declare the getUserID method, which... gets the user ID. An integer
 
-    String getRole();
-    
-    void setRole(String role);
+    void decrementUserID(); // Method to decrement the user ID, used to ensure a linear ID number order when removing users.
+
+    String getUsername(); // Similarly, gets the username. A string
+
+    void setUsername(String username); // Method to set the username, taking a string.
+
+    String getPassword(); // Gets the password, a string
+
+    void setPassword(String password); // Method to set the password, taking a string.
+
+    String getRole(); // Gets the user role, a string
+
+    void setRole(String role); // Method to set the user role, taking a string.
 }
