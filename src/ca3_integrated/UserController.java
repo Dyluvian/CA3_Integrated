dcffef41cc2ad49b8784f6cfe41ce4b90c3cf49a
Dyluvian <152575690+Dyluvian@users.noreply.github.com>
2024-03-27 09:39:30 +0000

    User Controller methods
    Add methods prototyped out to the User Controller, which is a straightforward construction

--- src/ca3_integrated/UserController.java
+++ src/ca3_integrated/UserController.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package ca3_integrated;
 
-/**
- *
- * @author User
- */
 public interface UserController {
-    
+    void login();
+    void showMenu(User user);
+    void logout();
 }
