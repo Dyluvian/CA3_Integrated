967a825d070f28a09ebb2fc91b91e427df976391
Dyluvian <152575690+Dyluvian@users.noreply.github.com>
2024-03-27 13:59:48 +0000

    UserController - comments
    Quick and breezy and get it out of the way

--- src/ca3_integrated/UserController.java
+++ src/ca3_integrated/UserController.java
@@ -7,12 +7,13 @@ package ca3_integrated;
 // ------------------
 // INTERFACE: USER CONTROLLER
 // ------------------
-// 
+// This interface is the user-facing presentation of the login, logout, and role-oriented menu functionalities.
+// It draws on the implementations defined in the User Controller Console (ConsoleUserController).
 // ------------------
 */
 
 public interface UserController {
-    void login();
-    void showMenu(User user);
-    void logout();
+    void login(); // Run the login method
+    void showMenu(User user); // Show the menu corresponding to the user in question
+    void logout(); // Run the logout method
 }
