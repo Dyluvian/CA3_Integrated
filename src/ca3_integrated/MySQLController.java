822f613d7a70750ce0be93ed9a306d4b15d4c05e
Dyluvian <152575690+Dyluvian@users.noreply.github.com>
2024-03-27 19:33:20 +0000

    MySQLConnector - initial setup
    Basics for the MySQL connector, including connections

--- src/ca3_integrated/MySQLController.java
+++ src/ca3_integrated/MySQLController.java
@@ -11,6 +11,16 @@ package ca3_integrated;
 // ------------------
 */
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class MySQLController {
+
+    private final String DB_URL = "jdbc:mysql://localhost/innistown_college";
+    private final String USERNAME = "pooa2024";
+    private final String PASSWORD = "pooa2024";
     
 }
