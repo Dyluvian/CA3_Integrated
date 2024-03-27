4f713c827d15ca1a488d1c3c5847fadeb674204c
Dyluvian <152575690+Dyluvian@users.noreply.github.com>
2024-03-27 09:52:00 +0000

    Lecturer basic docstring
    Basic docstring for Lecturer so that I remember who I am and what I'm doing

--- src/ca3_integrated/Lecturer.java
+++ src/ca3_integrated/Lecturer.java
@@ -1,5 +1,16 @@
 package ca3_integrated;
 
+/*
+// Dylan Geraghty - CA3 (Integrated CA) - Programming: Object-Oriented Approach
+// ------------------
+// https://github.com/Dyluvian/CA3_Integrated
+// ------------------
+// CLASS: LECTURER, SUBCLASS OF STAFF
+// ------------------
+//
+// ------------------
+*/
+
 public class Lecturer extends Staff {
 
     public Lecturer(int userID, String username, String password) {
