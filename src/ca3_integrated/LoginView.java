39bb55631fe99ee0b8f3b15c3704b3fa77a267fc
Dyluvian <152575690+Dyluvian@users.noreply.github.com>
2024-03-27 10:03:43 +0000

    LoginView basic docstring
    Same again

--- src/ca3_integrated/LoginView.java
+++ src/ca3_integrated/LoginView.java
@@ -1,5 +1,16 @@
 package ca3_integrated;
 
+/*
+// Dylan Geraghty - CA3 (Integrated CA) - Programming: Object-Oriented Approach
+// ------------------
+// https://github.com/Dyluvian/CA3_Integrated
+// ------------------
+// INTERFACE: LOGIN VIEW
+// ------------------
+// 
+// ------------------
+*/
+
 public interface LoginView {
 
     void displayLoginPrompt(); // Run the method to display the login prompt.
