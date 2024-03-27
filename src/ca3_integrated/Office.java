503a575e6684dca4526b6c6c00a8004ada8af37d
Dyluvian <152575690+Dyluvian@users.noreply.github.com>
2024-03-27 09:52:50 +0000

    Office basic docstring
    Same again

--- src/ca3_integrated/Office.java
+++ src/ca3_integrated/Office.java
@@ -1,5 +1,16 @@
 package ca3_integrated;
 
+/*
+// Dylan Geraghty - CA3 (Integrated CA) - Programming: Object-Oriented Approach
+// ------------------
+// https://github.com/Dyluvian/CA3_Integrated
+// ------------------
+// CLASS: OFFICE, SUBCLASS OF STAFF
+// ------------------
+//
+// ------------------
+*/
+
 public class Office extends Staff {
 
     public Office(int userID, String username, String password) {
