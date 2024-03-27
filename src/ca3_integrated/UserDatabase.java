1e5fcfb64d2c625f5c368e5aabbfbb85f7b797e7
Dyluvian <152575690+Dyluvian@users.noreply.github.com>
2024-03-27 08:49:11 +0000

    User Database constructor
    Add constructor for User Database, which contains an arraylist of users, and add the prescribed initial user to the list.

--- src/ca3_integrated/UserDatabase.java
+++ src/ca3_integrated/UserDatabase.java
@@ -1,13 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ca3_integrated;
 
-/**
- *
- * @author User
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserDatabase {
+
+    private List<User> users;
+
+    public UserDatabase() {
+        users = new ArrayList<>();
+        users.add(new Admin(1, "admin", "java"));
+    }
     
 }
