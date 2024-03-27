11871f73a7663e9487c3b73f65c2e290b79062e2
Dyluvian <152575690+Dyluvian@users.noreply.github.com>
2024-03-27 09:50:34 +0000

    Admin basic docstring
    While I am at it, let's throw in initial docstrings for some of the other methods

--- src/ca3_integrated/Admin.java
+++ src/ca3_integrated/Admin.java
@@ -2,6 +2,17 @@ package ca3_integrated;
 
 public class Admin extends Staff {
 
+/*
+// Dylan Geraghty - CA3 (Integrated CA) - Programming: Object-Oriented Approach
+// ------------------
+// https://github.com/Dyluvian/CA3_Integrated
+// ------------------
+// CLASS: ADMIN, SUBCLASS OF STAFF
+// ------------------
+//
+// ------------------
+*/
+
     public Admin(int userID, String username, String password) {
         super(userID, username, password, "Admin");
     }
