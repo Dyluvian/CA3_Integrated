ca0a995ad501edcbe46d50c00945afddf808e76c
Dyluvian <152575690+Dyluvian@users.noreply.github.com>
2024-03-27 19:40:05 +0000

    Create CourseReport
    I am splitting out each report type as its own entity, for modularity.

---
+++ src/ca3_integrated/CourseReport.java
@@ -0,0 +1,19 @@
+package ca3_integrated;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class CourseReport {
+
+    private final String DB_URL = "jdbc:mysql://localhost/innistown_college";
+    private final String USERNAME = "pooa2024";
+    private final String PASSWORD = "pooa2024";
+
+    private ReportFormatter reportFormatter;
+
+    public CourseReport(ReportFormatter reportFormatter) {
+        this.reportFormatter = reportFormatter;
+    }
