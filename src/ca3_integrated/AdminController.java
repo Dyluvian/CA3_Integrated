b67d42224060c2f9a742aa2c8f68b3ef53f73232
Dyluvian <152575690+Dyluvian@users.noreply.github.com>
2024-03-27 12:42:34 +0000

    AdminController - showAdminMenu, prelude
    The opening section of the AdminMenu, with six discrete parts to come

--- src/ca3_integrated/AdminController.java
+++ src/ca3_integrated/AdminController.java
@@ -11,6 +11,8 @@ package ca3_integrated;
 // ------------------
 */
 
+import java.util.Scanner;
+
 public class AdminController {
     
     private UserDatabase userDatabase;
@@ -29,5 +31,13 @@ public class AdminController {
             showAdminMenu((Admin) user);
         }
     }
+    
+    private void showAdminMenu(Admin admin) {
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+        do {
+            System.out.println("---\nInput 1 to view current users.\nInput 2 to add new users.\nInput 3 to modify the usernames, passwords, and roles of other users.\nInput 4 to delete users.\nInput 5 to log out.\nInput 6 to close the software.\n---");
+            choice = scanner.nextInt();
+            switch (choice) {
   
 }
